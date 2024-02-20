e03f2b32c3dac49e0e7a6f94c8730097866f8df1
stevenbutz <stephbutzz@gmail.com>
2024-02-20 15:32:54 -0500

    Added everything needed to run 'npm start' in terminal to run the react app after pulling

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import LoginForm from './LoginPage/LoginForm';
+import logo from './LoginPage/FitInc.png'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm logo={logo} />
+    </div>
+  );
+}
+
+export default App;
+
