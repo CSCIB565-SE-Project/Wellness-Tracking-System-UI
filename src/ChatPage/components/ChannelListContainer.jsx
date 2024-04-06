e73aadf4a1e9c7716d9f572bdefb01ac3962fe27
stebutz <stebutz@iu.edu>
2024-04-06 19:00:49 -0400

    exit chat button

--- src/ChatPage/components/ChannelListContainer.jsx
+++ src/ChatPage/components/ChannelListContainer.jsx
@@ -1,7 +1,8 @@
-//ChannelListContainer.jsx
 import React, { useState } from 'react';
 import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
+import { useNavigate } from 'react-router-dom';
+
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png'
@@ -9,7 +10,7 @@ import LogoutIcon from '../assets/logout.png'
 
 const cookies = new Cookies();
 
-const SideBar = ({ logout }) => (
+const SideBar = ({ exitChat }) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
@@ -17,7 +18,7 @@ const SideBar = ({ logout }) => (
             </div>
         </div>
         <div className="channel-list__sidebar__icon2">
-            <div className="icon1__inner" onClick={logout}>
+            <div className="icon1__inner" onClick={exitChat}>
                 <img src={LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
@@ -40,24 +41,41 @@ const customChannelMessagingFilter = (channels) => {
 
 const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEditing, setToggleContainer }) => {
     const { client } = useChatContext();
+    const navigate = useNavigate();
 
-    const logout = () => {
-        cookies.remove("token");
-        cookies.remove('userId');
-        cookies.remove('username');
-        cookies.remove('fullName');
-        cookies.remove('avatarURL');
-        cookies.remove('hashedPassword');
-        cookies.remove('phoneNumber');
-
-        window.location.reload();
-    }
+    const exitChat = () => {
+        const userData = localStorage.getItem('user');
+        if (userData) {
+            const user = JSON.parse(userData);
+            // Determine the dashboard path based on user role
+            let dashboardPath = '/'; // Default path for unrecognized roles
+            switch (user.role.toLowerCase()) {
+                case 'user':
+                    dashboardPath = '/userdashboard';
+                    break;
+                case 'professional':
+                    dashboardPath = '/professionaldashboard';
+                    break;
+                case 'admin':
+                    dashboardPath = '/admindashboard';
+                    break;
+                default:
+                    // Optionally handle unknown role or implement a default case
+                    break;
+            }
+    
+            navigate(dashboardPath); // Navigate to the determined path
+        } else {
+            navigate('/login'); // If no user data is found, navigate to login or a default route
+        }
+    };
+    
 
     const filters = { members: { $in: [client.userID] } };
 
     return (
         <>
-            <SideBar logout={logout} />
+            <SideBar exitChat={exitChat} />
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
                 <ChannelSearch setToggleContainer={setToggleContainer} />
